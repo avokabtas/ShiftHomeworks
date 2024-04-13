8c31a5cc4bf65100c436b028fe96660f7d34bc33
Aliia Satbakova <aliiasatbakova@MacBook-Air.local>
2024-04-13 20:52:46 +0700

    feat: декомпозиция, доработан год выпуска

---
+++ HW1/HW1/Body.swift
@@ -0,0 +1,18 @@
+//
+//  Body.swift
+//  HW1
+//
+//  Created by Aliia Satbakova  on 13.04.2024.
+//
+
+import Foundation
+
+enum Body: String, CaseIterable {
+    case hatchback = "Хэтчбек"
+    case microCompactCar = "Малолитражный автомобиль"
+    case stationWagon = "Универсал"
+    case van = "Фургон"
+    case minivan = "Минивэн"
+    case sedan = "Седан"
+    case crossover = "Кроссовер"
+}
---
+++ HW1/HW1/ErrorMessage.swift
@@ -0,0 +1,16 @@
+//
+//  ErrorMessage.swift
+//  HW1
+//
+//  Created by Aliia Satbakova  on 13.04.2024.
+//
+
+import Foundation
+
+enum ErrorMessage: String {
+    case enterNumber = "Ошибка: введите число!"
+    case menuNumber = "Ошибка: введите число согласно меню с 1 - 4!"
+    case fieldIsRequired = "Ошибка: данное поле обязательно для заполнения!"
+    case incorrectBodyType = "Ошибка: неверный выбор типа кузова!"
+    case incorrectYear = "Ошибка: неверный год выпуска!"
+}
