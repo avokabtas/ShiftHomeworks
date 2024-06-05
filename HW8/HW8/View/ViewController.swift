aba45b957cb6da44c5f4764b2297571b9d28970f
Aliia Satbakova <aliiasatbakova@MacBook-Air.local>
2024-06-05 13:14:02 +0700

    init: добавлен пустой проект

---
+++ HW8/HW8/View/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW8
+//
+//  Created by Aliia Satbakova  on 05.06.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .cyan
+    }
+
+
+}
+
